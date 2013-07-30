8159c8890f04a12e2cf0e3fb1a3537e71fd309f3
terrence pietrondi <terrence.a.pietrondi@gmail.com>
2013-07-30 12:19:33 -0400

    templating the site

---
+++ static/site.js
@@ -0,0 +1,16 @@
+var nav_path = window.location.pathname.substring(1,window.location.pathname.length);
+if(nav_path == ""){
+    nav_path = "/";
+}
+
+$("ul.nav > li > a").each(function(e){ 
+    if( $(this).attr("href") == nav_path ){
+        $(this).parent().addClass("active");
+    } else {
+        $(this).parent().removeClass("active");
+    } 
+});
+
+if( $('.carousel').length > 0) {
+    $('.carousel').carousel();
+}

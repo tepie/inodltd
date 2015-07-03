1631eb914a7092b7979dc8d79f80289e989939ce
Terry Pietrondi <tpietrondi@crossview.com>
2015-07-03 16:12:58 -0400

    new media

--- scripts/resize_media_images.sh
+++ scripts/resize_media_images.sh
@@ -5,18 +5,8 @@ find ../media -name "*.png" | grep -v "\-low" | grep -v "\-med" |  grep -v "\-th
 source exec_resize.sh
 rm exec_resize.sh
 
-# med jpg
-find ../media -name "*.jpg" | grep -v "\-low" | grep -v "\-med" |  grep -v "\-thumb" | sed 's/\.jpg$//' | awk -F ' ' '{print "convert "$1".jpg -resize 50% "$1"-med.jpg"}' > exec_resize.sh
-source exec_resize.sh
-rm exec_resize.sh
-
 # low png
 find ../media -name "*.png" | grep -v "\-low" | grep -v "\-med" |  grep -v "\-thumb" | sed 's/\.png$//' | awk -F ' ' '{print "convert "$1".png -resize 30% "$1"-low.png"}' > exec_resize.sh
-source exec_resize.sh
-rm exec_resize.sh
-
-#low jpg
-find ../media -name "*.jpg" | grep -v "\-low" | grep -v "\-med" |  grep -v "\-thumb" | sed 's/\.jpg$//' | awk -F ' ' '{print "convert "$1".jpg -resize 30% "$1"-low.jpg"}' > exec_resize.sh
 source exec_resize.sh
 rm exec_resize.sh
 
@@ -25,7 +15,11 @@ find ../media -name "*.png" | grep -v "\-low" | grep -v "\-med" |  grep -v "\-th
 source exec_resize.sh
 rm exec_resize.sh
 
+#low jpg
+find . -name "*.jpg" | grep -v "\-low" | grep -v "\-med" |  grep -v "\-thumb" | sed 's/\.jpg$//' | awk -F ' ' '{print "convert "$1".jpg -resize 30% "$1"-low.jpg"}' | sh
+
+# med jpg
+find . -name "*.jpg" | grep -v "\-low" | grep -v "\-med" |  grep -v "\-thumb" | sed 's/\.jpg$//' | awk -F ' ' '{print "convert "$1".jpg -resize 50% "$1"-med.jpg"}' | sh
+
 #thumb jpg
-find ../media -name "*.jpg" | grep -v "\-low" | grep -v "\-med" | grep -v "\-thumb" | sed 's/\.jpg$//' | awk -F ' ' '{print "convert "$1".jpg -thumbnail 400x300! "$1"-thumb.jpg"}' > exec_resize.sh
-source exec_resize.sh
-rm exec_resize.sh
+find . -name "*.jpg" | grep -v "\-low" | grep -v "\-med" | grep -v "\-thumb" | sed 's/\.jpg$//' | awk -F ' ' '{print "convert "$1".jpg -thumbnail 400x300! "$1"-thumb.jpg"}' | sh
